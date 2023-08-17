9dc900e5b8cba3455f162626b658961cdb0916dd
Ivan Yadchyshyn <yad7devadress@gmail.com>
2023-08-17 22:56:11 +0300

    Created database for an application, added Category table to DB and created Controller and View for Category

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PetProject.Models;
+
+namespace PetProject.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            :base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
