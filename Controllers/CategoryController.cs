536b7bfc072249788ea3b6a86f26810bd31b0f92
Ivan Yadchyshyn <yad7devadress@gmail.com>
2023-08-18 18:41:31 +0300

    made categories to display on web-page

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,12 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PetProject.Data;
+using PetProject.Models;
 
 namespace PetProject.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            List<Category> categoryList = _context.Categories.ToList();
+            return View(categoryList);
         }
     }
 }
--- Migrations/20230818151458_SeedCategoryTable.Designer.cs
+++ Migrations/20230818151458_SeedCategoryTable.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PetProject.Data;
 
@@ -10,9 +11,11 @@ using PetProject.Data;
 namespace PetProject.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230818151458_SeedCategoryTable")]
+    partial class SeedCategoryTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
