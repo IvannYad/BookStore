8f7f692b7d70d54fcb115371330ddefeab3a736e
Ivan Yadchyshyn <yad7devadress@gmail.com>
2023-08-20 23:12:01 +0300

    added 'Edit' and 'Delete' buttons to 'Caregory' web-page

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -16,7 +16,7 @@ namespace PetProject.Controllers
 
         public IActionResult Index()
         {
-            List<Category> categoryList = _context.Categories.ToList();
+            List<Category> categoryList = _context.Categories.OrderBy(c => c.DisplayOrder).ToList();
             return View(categoryList);
         }
 
@@ -44,6 +44,75 @@ namespace PetProject.Controllers
             }
 
             return View();
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id is null or 0)
+            {
+                return NotFound();
+            }
+
+            Category? categoryToEdit = _context.Categories.Find(id);
+            if (categoryToEdit is null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryToEdit);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            if (category.Name is not null && !Regex.IsMatch(category.Name, @"^[a-zA-Z]+$"))
+            {
+                ModelState.AddModelError("Name", "'Category Name' must only consists of latin letters");
+            }
+
+            if (category.Name is not null && !char.IsUpper(category.Name[0]))
+            {
+                ModelState.AddModelError("Name", "'Category Name' must start with capital letter");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _context.Categories.Update(category);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id is null or 0)
+            {
+                return NotFound();
+            }
+
+            Category? categoryToDelete = _context.Categories.Find(id);
+            if (categoryToDelete is null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryToDelete);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePost(int? id)
+        {
+            Category categoryToDelete = _context.Categories.Find(id);
+            if (categoryToDelete is null)
+            {
+                return NotFound();
+            }
+
+            _context.Categories.Remove(categoryToDelete);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
