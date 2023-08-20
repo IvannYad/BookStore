3add876e17a0a6458177d1264e2d97e42ff3dd35
Ivan Yadchyshyn <yad7devadress@gmail.com>
2023-08-20 14:28:23 +0300

    added client-side and server-side validation for input on 'Create Category'

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using PetProject.Data;
 using PetProject.Models;
+using System.Text.RegularExpressions;
 
 namespace PetProject.Controllers
 {
@@ -17,6 +18,32 @@ namespace PetProject.Controllers
         {
             List<Category> categoryList = _context.Categories.ToList();
             return View(categoryList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            if (category.Name is not null && !Regex.IsMatch(category.Name, @"^[a-zA-Z]+$"))
+            {
+                ModelState.AddModelError("Name","'Category Name' must only consists of latin letters");
+            }
+            if (category.Name is not null && !char.IsUpper(category.Name[0]))
+            {
+                ModelState.AddModelError("Name", "'Category Name' must start with capital letter");
+            }
+            if (ModelState.IsValid)
+            {
+                _context.Categories.Add(category);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View();
         }
     }
 }
--- Models/Category.cs
+++ Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace PetProject.Models
 {
@@ -6,8 +7,14 @@ namespace PetProject.Models
     {
         [Key]
         public int Id { get; set; }
+        
         [Required]
+        [DisplayName("Category Name")]
+        [MaxLength(30)]
         public string Name { get; set; }
+        
+        [DisplayName("Display Order")]
+        [Range(1,100)]
         public int DisplayOrder { get; set; }
     }
 }
