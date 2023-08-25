9ba9ad92d5ff5cb00880e504dd147d59ed3872a6
Ivan Yadchyshyn <yad7devadress@gmail.com>
2023-08-25 17:17:20 +0300

    implemented IRepository interface with Repository class

--- PetProject.DataAccess/Repository/IRepository/IRepository.cs
+++ PetProject.DataAccess/Repository/IRepository/IRepository.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace PetProject.DataAccess.Repository.IRepository
 {
-    internal class IRepository
+    internal interface IRepository<T> where T : class
     {
+        IEnumerable<T> GetAll();
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T item);
+        void Remove(T item);
+        void RemoveRange(IEnumerable<T> entities);
     }
 }
---
+++ PetProject.DataAccess/Repository/Repository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PetProject.DataAccess.Data;
+
+namespace PetProject.DataAccess.Repository
+{
+    public class Repository<T> : IRepository.IRepository<T>
+        where T : class
+    {
+        private ApplicationDbContext _context;
+        private DbSet<T> _dbSet;
+
+        public Repository(ApplicationDbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+        public IEnumerable<T> GetAll()
+        {
+            return _dbSet;
+        }
+        public T Get(Expression<Func<T, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveRange(IEnumerable<T> entities)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
