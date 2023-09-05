f7c38ec65520a9e70136a7182fa280acad47dfd1
Ivan Yadchyshyn <yad7devadress@gmail.com>
2023-09-05 21:17:12 +0300

    make Identity framewort register out custom ApplicationUser rather than IdentityUser

--- PetProject/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ PetProject/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -18,6 +18,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.Logging;
+using PetProject.Models;
 
 namespace PetProject.Areas.Identity.Pages.Account
 {
@@ -154,11 +155,11 @@ namespace PetProject.Areas.Identity.Pages.Account
             return Page();
         }
 
-        private IdentityUser CreateUser()
+        private ApplicationUser CreateUser()
         {
             try
             {
-                return Activator.CreateInstance<IdentityUser>();
+                return Activator.CreateInstance<ApplicationUser>();
             }
             catch
             {
