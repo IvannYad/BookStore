e224f24330b14dfa5572107193d6d79c476954ea
Ivan Yadchyshyn <yad7devadress@gmail.com>
2023-09-19 11:01:45 +0300

    add Companies table to database

--- PetProject.DataAccess/Data/ApplicationDbContext.cs
+++ PetProject.DataAccess/Data/ApplicationDbContext.cs
@@ -20,6 +20,7 @@ namespace PetProject.DataAccess.Data
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public DbSet<Company> Companies { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
---
+++ PetProject.Models/Company.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetProject.Models
+{
+    [Table("Companies")]
+    public class Company
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [DisplayName("Company Name")]
+        public string Name { get; set; }
+
+        [DisplayName("Company Address")]
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+
+        [DisplayName("Postal Code")]
+        public string PostalCode { get; set; }
+
+        [DisplayName("Phone Number")]
+        public string PhoneNumber { get; set; }
+    }
+}
