b3c930cf309b62c494cd9df770371a07a2d8f6dd
Ivan Yadchyshyn <yad7devadress@gmail.com>
2023-09-20 18:58:04 +0300

    add CompanyToUser relationship

--- PetProject.Models/ApplicationUser.cs
+++ PetProject.Models/ApplicationUser.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,5 +18,10 @@ namespace PetProject.Models
         public string? City { get; set; }
         public string? State { get; set; }
         public string? PostalCode { get; set; }
+
+        [ForeignKey("Company")]
+        public int? CompanyId { get; set; }
+        [ValidateNever]
+        public Company? Company { get; set; }
     }
 }
