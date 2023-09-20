28a4a460b956d29aba3d3659fbc3087dadea1401
Ivan Yadchyshyn <yad7devadress@gmail.com>
2023-09-20 19:31:54 +0300

    make Company user register with CompanyId populated

--- PetProject/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ PetProject/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -171,6 +171,11 @@ namespace PetProject.Areas.Identity.Pages.Account
                 user.PostalCode = Input.PostalCode;
                 user.State = Input.State;
                 user.PhoneNumber = Input.PhoneNumber;
+
+                if (Input.Role == SD.Role_Company)
+                {
+                    user.CompanyId = Input.CompanyId;
+                }
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
                 if (result.Succeeded)
