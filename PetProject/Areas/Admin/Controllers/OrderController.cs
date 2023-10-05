014dae2264105d7b77c7e79e62d720d512f034fb
Ivan Yadchyshyn <yad7devadress@gmail.com>
2023-10-06 00:10:38 +0300

    feat: create OrderController; create OrderVM

---
+++ PetProject/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PetProject.DataAccess.Repository;
+using PetProject.Models;
+
+namespace PetProject.Areas.Admin.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly UnitOfWork _unitOfWork;
+
+        public OrderController(UnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region API CALLS
+
+        // Method for retrieving all entities from db, and returning JSON file with data entites, that is 
+        // displayed in DataTable on Index page.
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<OrderHeader> orderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+            return Json(new { data = orderHeaders });
+        }
+
+        #endregion
+    }
+}
