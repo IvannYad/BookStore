bba2067873af8cfbf81189f84fb5a24ee9aa019a
Ivan Yadchyshyn <yad7devadress@gmail.com>
2023-10-09 19:11:03 +0300

    feat: create IDbInitializer; create DbInitializer

---
+++ PetProject.DataAccess/DbInitializer/IDbInitializer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetProject.DataAccess.DbInitializer
+{
+    public interface IDbInitializer
+    {
+        /// <summary>
+        ///     Method for creating admin user and roles
+        /// </summary>
+        void Initialize();
+    }
+}
