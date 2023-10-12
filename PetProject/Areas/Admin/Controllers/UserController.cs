4b1dcf076720d8694ad683053575aae00f0fdea7
Ivan Yadchyshyn <yad7devadress@gmail.com>
2023-10-12 21:27:17 +0300

    feat: make display list of users; make display company name

---
+++ PetProject/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PetProject.DataAccess.Data;
+using PetProject.DataAccess.Repository.IRepository;
+using PetProject.Models;
+using PetProject.Utility;
+using SQLitePCL;
+using System.Text.RegularExpressions;
+
+namespace PetProject.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    // Make sure that only user with role admin can access all action methods of controller.
+    [Authorize(Roles = SD.Role_Admin + "," + SD.Role_Employee)]
+    public class UserController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        
+        #region API CALLS
+
+        // Method for retrieving all entities from db, and returning JSON file with data entites, that is 
+        // displayed in DataTable on Index page.
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<ApplicationUser> userList = _context.ApplicationUsers.Include(u => u.Company).OrderBy(p => p.Name).ToList();
+            foreach (var user in userList)
+            {
+                if (user.Company is null)
+                    user.Company = new Company() { Name = "" };
+            }
+            return Json(new { data = userList });
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int? id)
+        {
+            return Json(new { success = true, message = "Deleting successfull" });
+        }
+
+        #endregion
+    }
+}
---
+++ PetProject/wwwroot/js/user.js
@@ -0,0 +1,35 @@
+﻿// Javascript code for some Users functions.
+
+var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+// Function for displaying DataTable.
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": { url: '/admin/user/getall' },
+        // Number of columns cannot exceed number of columns in cshtml document(<th> elements).
+        "columns": [
+            { data: 'name', "width": "25%" },
+            { data: 'email', "width": "15%" },
+            { data: 'phoneNumber', "width": "10%" },
+            { data: 'company.name', "width": "15%" },
+            { data: '', "width": "10%" },
+            {
+                data: 'id', 
+                "render": function (data) {
+                    return `<div class="w-75 btn-group" role="group">
+                    <a href="/admin/product/upsert?id=${data}" class="btn btn-primary mx-2"><i class="bi bi-pencil-square"></i> Edit</a>
+                    </div>`
+                },
+                "width": "25%" 
+            }
+        ]
+    });
+}
+
+
+
+
