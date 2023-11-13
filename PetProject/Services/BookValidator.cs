c1cda5aa03a402304a6baaa516ed46839bef7811
Ivan Yadchyshyn <yad7devadress@gmail.com>
2023-11-13 13:02:52 +0200

    feat: add BookValidator

---
+++ PetProject/Services/BookValidator.cs
@@ -0,0 +1,19 @@
+﻿using PetProject.Models;
+using PetProject.Models.ViewModels;
+using PetProject.Services.Interfaces;
+
+namespace PetProject.Services
+{
+    public class BookValidator : IValidator<Product>
+    {
+        public bool Validate(Product item)
+        {
+            if (item.Title is not null && !char.IsUpper(item.Title[0]))
+            {
+                // throw new InvalidBookValidationException("'Book Title' must start with capital letter");
+            }
+
+            return true;
+        }
+    }
+}
