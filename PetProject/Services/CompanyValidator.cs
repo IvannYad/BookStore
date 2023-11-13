1df82be5bed8f976dc7945c87f2b0d064a363372
Ivan Yadchyshyn <yad7devadress@gmail.com>
2023-11-13 18:57:18 +0200

    feat: add CompanyValidator; implement Validate() of CompanyValidator

---
+++ PetProject/Services/CompanyValidator.cs
@@ -0,0 +1,27 @@
+﻿using PetProject.Models;
+using PetProject.Models.ViewModels;
+using PetProject.Services.Interfaces;
+using PetProject.Utility.CustomExceptions;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+using System.Text.RegularExpressions;
+
+namespace PetProject.Services
+{
+    public class CompanyValidator : IValidator<Company>
+    {
+        public bool Validate(Company item)
+        {
+            if (item.Name is not null && !Regex.IsMatch(item.Name, @"^[a-zA-Z]+$"))
+            {
+                // throw new CompanyValidationException("'Company Name' must only consists of latin letters");
+            }
+
+            if (item.Name is not null && !char.IsUpper(item.Name[0]))
+            {
+                // throw new CompanyValidationException("'Company Name' must start with capital letter");
+            }
+
+            return true;
+        }
+    }
+}
